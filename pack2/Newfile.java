fe4121301cb22d3c38d40192ae20129f5c3b9391
Ramanujam-p <pramanujam68@gmail.com>
2025-08-23 23:26:53 +0530

    Start tracking pack1 and pack2 directories

---
+++ pack2/Newfile.java
@@ -0,0 +1,14 @@
+package pack2;
+import pack1.Access;
+public class Newfile extends Access {
+    public void print()
+    {
+        System.out.println("the value of public and protected access from another package:"+a+","+c);
+
+    }
+    public static void main(String[] args) {
+        Newfile obj2 = new Newfile();
+        obj2.print();
+    }
+}
+

b1f4d65da3d4413d731adaffc1fb95bf557d4ec5
Ramanujam-p <pramanujam68@gmail.com>
2025-08-27 12:20:48 +0530

    my turn

--- week1programs/earray.java
+++ week1programs/earray.java
@@ -1,36 +1,33 @@
 package week1programs;
 import java.util.*;
-public class earray {
+class earray
+{
     public static void main(String[] args)
     {
-        // give a java program that find the even elements between two array and create a third array with those even elements
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter the number of elements in the first array:");
-        int n = sc.nextInt();               
-        int[] a = new int[n];
-        for(int i=0;i<n;i++)
-            a[i] = sc.nextInt();
-        System.out.println("enter the number of elements in the second array:");
+        System.out.println("enter the size of the first array:");
         int m = sc.nextInt();
-        int[] b = new int[m];
+        int[] a = new int[m];
+        System.out.println("enter the array elements:");
         for(int i=0;i<m;i++)
+            a[i] = sc.nextInt();
+        System.out.println("enter the size of the second array:");
+        int n = sc.nextInt();
+        int[] b = new int[n];
+        System.out.println("enter the array elements:");
+        for(int i=0;i<n;i++)
             b[i] = sc.nextInt();
-        ArrayList<Integer> evenList = new ArrayList<>();
-        for(int num : a) {  
-            if(num % 2 == 0) {
-                evenList.add(num);
-            }
-        }
-        for(int num : b) {
-            if(num % 2 == 0) {
-                evenList.add(num);
-            }
-        }
-        int[] evenArray = new int[evenList.size()];
-        for(int i = 0; i < evenList.size(); i++) {
-            evenArray[i] = evenList.get(i);
-        }
-        System.out.println("The even elements array: " + Arrays.toString(evenArray));
-        sc.close();  
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int num:a)
+            if(num%2==0)
+                list.add(num);
+        for(int num:b)
+            if(num%2==0)
+                list.add(num);
+        int[] c = new int[list.size()];
+        for(int i=0;i<list.size();i++)
+            c[i] = list.get(i);
+        System.out.println("The even array is:"+Arrays.toString(c));
+        sc.close();
     }
 }

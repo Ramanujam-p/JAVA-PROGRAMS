048dfc68a1cb10515b416e9a9b3bbd14a501a6b7
Ramanujam-p <pramanujam68@gmail.com>
2025-08-27 10:38:30 +0530

    operators

---
+++ week1programs/op.java
@@ -0,0 +1,23 @@
+package week1programs;
+import java.util.*;
+public class op {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int runs;
+        byte fouls;
+        boolean status = false;
+        System.out.println("enter the number of runs and fouls and made by the team");
+        runs = sc.nextInt();
+        fouls = sc.nextByte();
+        if(runs>=50 && fouls<=5)
+        {
+            status = true;
+            System.out.println("The win status is:"+status);
+        }
+        else
+             System.out.println("The win status is:"+status);
+        System.out.println("runs:"+runs+"fouls:"+fouls);
+        sc.close();
+    }
+}

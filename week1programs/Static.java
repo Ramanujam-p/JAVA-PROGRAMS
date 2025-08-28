06ed55a04371ae00d717c4cb926953446ba425e4
Ramanujam-p <pramanujam68@gmail.com>
2025-08-28 10:31:16 +0530

    static block

---
+++ week1programs/Static.java
@@ -0,0 +1,28 @@
+package week1programs;
+public class Static {
+    static int a;
+    static
+    {
+        System.out.println("Static block executed");
+        a=10;
+    }
+    static void method()
+    {
+        System.out.println("Static method");
+    }
+    Static()
+    {
+        System.out.println("Constructor executed");
+    }
+    void display()
+    {
+        System.out.println("Non static method");
+    }
+    public static void main(String[] args)
+    {
+      System.out.println(a);
+      method();
+      Static s = new Static();
+      s.display();
+    }
+}

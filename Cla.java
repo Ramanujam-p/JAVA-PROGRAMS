229566b3fe0ec1823119e9a41d7e1d4648dfff62
Ramanujam-p <pramanujam68@gmail.com>
2025-09-23 19:07:31 +0530

    how to input the comment line arguments during run the program

---
+++ Cla.java
@@ -0,0 +1,10 @@
+class Cla
+{
+    public static void main(String[] args)
+    {
+        if(args.length>0) 
+        System.out.println("present");
+        for(String a:args)
+            System.out.println(a);  
+    }
+}

f1f25f8128c6e16c9b1b5190bc9d687ee29edf8a
Ramanujam-p <pramanujam68@gmail.com>
2025-09-24 21:05:12 +0530

    checked exceptions in java

---
+++ CheckedExample.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class CheckedExample {
+    public static void main(String[] args) throws IOException {
+        FileReader file = new FileReader("nonexistent.txt");
+         // may throw FileNotFoundException
+         file.close();
+    }
+}

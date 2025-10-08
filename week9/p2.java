c9e394fe749c7dadf541636f881b9bc64f7cda56
Ramanujam-p <pramanujam68@gmail.com>
2025-10-08 21:17:35 +0530

    Runnable class in java

---
+++ week9/p2.java
@@ -0,0 +1,26 @@
+package week9;
+
+class A implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("I am class A");
+    }
+}
+
+class B implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("I am class B");
+    }
+}
+
+public class p2 {
+    public static void main(String[] args) {
+        A a = new A();
+        B b = new B();
+        Thread t1 = new Thread(a);
+        Thread t2 = new Thread(b);
+        t1.start();
+        t2.start();
+    }
+}

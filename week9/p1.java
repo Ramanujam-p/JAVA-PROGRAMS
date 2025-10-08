f2423d2deec0a1b2211ef84eaa283bcb8105cb44
Ramanujam-p <pramanujam68@gmail.com>
2025-10-08 21:14:52 +0530

    Thread concept

---
+++ week9/p1.java
@@ -0,0 +1,38 @@
+package week9;
+
+class add extends Thread {
+    int x, y;
+
+    add(int a, int b) {
+        x = a;
+        y = b;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("sum is:" + (x + y));
+    }
+}
+
+class sub extends Thread {
+    int x, y;
+
+    sub(int a, int b) {
+        x = a;
+        y = b;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("sub is:" + (x - y));
+    }
+}
+
+public class p1 {
+    public static void main(String[] args) {
+        add obj = new add(5, 2);
+        sub o = new sub(5, 2);
+        obj.start();
+        o.start();
+    }
+}
